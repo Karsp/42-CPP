7b7e26f2c297deabef481d89f3205a51d973809c
David Aviles Leiva <dm.daviles@gmail.com>
2024-10-07 17:30:05 +0200

    Makefile working and base for exercise 1.

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -1,13 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: daviles- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/07 13:00:56 by daviles-          #+#    #+#             */
-/*   Updated: 2024/10/07 13:00:58 by daviles-         ###   ########.fr       */
+/*   Created: 2024/10/07 13:56:19 by daviles-          #+#    #+#             */
+/*   Updated: 2024/10/07 13:56:23 by daviles-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <iostream>
+#include <cstdio>
 
+void    printUpper(char *str)
+{
+    printf("%s\n",str);
+}
 
+int main(int argc, char **argv)
+{
+    // char    *str;
+    // char    *aux;
+
+    (void)argc;
+    // while (++argv)
+    // {
+    //     aux = strjoin(str, argv);
+    //     if(!aux)
+    // }
+    printUpper(argv[1]);
+    return (0);
+}
