8e97f9ec12d9878b38f56bfac834730e08f8625a
David <dm.daviles@gmail.com>
2024-10-28 14:49:40 +0100

    Working on Search, print fields with format. Check if options to show is valid.@

--- 00/ex01/Contact.cpp
+++ 00/ex01/Contact.cpp
@@ -9,5 +9,14 @@
 /*   Updated: 2024/10/10 16:57:03 by daviles-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "Contact.hpp"
 
+// void    Contact::print_contact()
+// {
+//     std::cout << "First Name " + this->_contact_list->first_name << std::endl;
+//     std::cout << "Last Name " + this->_contact_list->last_name << std::endl;
+//     std::cout << "NickName " + this->_contact_list->nickname << std::endl;
+//     std::cout << "Phone number" + this->_contact_list->phone_number << std::endl;
+//     std::cout << "Darkest secret " + this->_contact_list->darkest_secret << std::endl;
+// }
 
