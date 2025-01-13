3dedd26ad84ea2d54f94ac0cb5fb2caffba858b8
karsp <dm.daviles@gmail.com>
2025-01-13 17:11:33 +0100

    Update code.

--- 02/ex00/Fixed.hpp
+++ 02/ex00/Fixed.hpp
@@ -10,15 +10,8 @@ private:
 
 public:
 	Fixed(/* args */);
-	Fixed(const int nb);
-	Fixed(const float fl);
-
 	Fixed (Fixed& other);
 	Fixed& operator=(Fixed& rhs);
-
-	float toFloat( void ) const;
-	int toInt( void ) const;
-
 	~Fixed();
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
