902a3efac62f1c0869e62978114c8bc068ed27ec
Karsp <dm.daviles@gmail.com>
2025-05-19 17:41:09 +0200

    Working on ex01. First test failed.

---
+++ 07/ex01/iter.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+template <typename T> 
+void iter(T *x, size_t size, void (*f)(T &)) 
+{
+	if (!*x || !*f)
+		return ;
+	if (size > x.size())
+		return ;
+	for (size_t i = 0; i < size; i++)
+	{
+		f(x[i]);
+	}
+	
+   
+}
