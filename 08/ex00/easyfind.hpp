915d7dac8d0503623c29b36586ccc8b38e2a86e6
set <dm.daviles@gmail.com>
2025-05-20 20:22:38 +0200

    Working on ex00.

---
+++ 08/ex00/easyfind.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+template <typename T> 
+int* easyfind(T *arr, int key) 
+{
+	if (!x)
+		return ;
+
+	int i = 0;
+	for (size_t i = 0; i < arr.size(); ++i)
+	{
+		if (arr[i] == key)
+			return i;
+	}
+
+if (index >= _size)
+		throw std::out_of_range("Array index out of range");
+	
+   
+}
