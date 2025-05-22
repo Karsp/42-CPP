551a4dc268adb0878c6982341e920424c2cef233
set <dm.daviles@gmail.com>
2025-05-22 11:47:32 +0200

    Add comments of different approaches to call the function.

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
-
+// (*f)(U &)   //Restricts f to be a function pointer taking a U& parameter  //iter(arr, 4, &print);
+// const U& f  //Call it with makes possible to call it from any callable object f via a const referente. //iter(arr, 4, &print<int>);
 template <typename T, typename U> 
 void iter(T *x, size_t size, const U& f)
 {
-	if (!x || !f)
+	if (!x)
 		return ;
 	for (size_t i = 0; i < size; i++)
 	{
