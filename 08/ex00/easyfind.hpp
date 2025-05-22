5bca01882ce524fa1175ae4ef18357c5b20258e2
set <dm.daviles@gmail.com>
2025-05-22 12:09:18 +0200

    Found better way to implement template. Add main test.

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -3,28 +3,16 @@
 
 
 template <typename T> 
-typename T::iterator easyfind(T *container, int key) 
+typename T::iterator easyfind(T &container, int key) 
 {
-	typename T::iterator *it;
+	typename T::iterator it;
 
-	it = container.find(container.begin(), container.end(), key);
-	if (it != container.end())
-		return (it);
-	throw std::out_of_range("Key not found.");
+	it = std::find(container.begin(), container.end(), key);
+	if (it == container.end())
+		throw std::out_of_range("Key not found.");
+	return (it);
 
 
-	// if (!x)
-	// 	return ;
 
-// 	int i = 0;
-// 	for (size_t i = 0; i < arr.size(); ++i)
-// 	{
-// 		if (arr[i] == key)
-// 			return i;
-// 	}
-
-// if (index >= _size)
-// 		throw std::out_of_range("Array index out of range");
-	
    
 }
--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -13,13 +13,21 @@ int	main()
 	l.push_back(3);
 	l.push_back(4);
 	l.push_back(5);
+
 	std::list<int>::iterator i = l.begin();
 
+	std::cout << "<<<<<<<< Print first element >>>>>>>>>" << std::endl;
 	std::cout << *i << std::endl;
-	std::cout << *(easyFind(l, 4)) << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "<<<<<<<< Try to find key 4 >>>>>>>>>" << std::endl;
+	std::cout << *(easyfind(l, 4)) << std::endl;
+	std::cout << std::endl;
 	try
 	{
-		std::cout << *(easyFind(l, 10)) << std::endl;
+		std::cout << "<<<<<<<< Try to find key 10 >>>>>>>>>" << std::endl;
+		std::cout << *(easyfind(l, 10)) << std::endl;
+		std::cout << std::endl;
 	}
 	catch (const std::exception& e) 
 	{
